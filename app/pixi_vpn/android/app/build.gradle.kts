50d2e05933e8cd9ca6a3515c7066dec95c336412
snlike992-web <snlike992@gmail.com>
2026-01-20 19:14:31 +0800

    chore(android): set minSdkVersion to 23

--- app/pixi_vpn/android/app/build.gradle.kts
+++ app/pixi_vpn/android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         applicationId = "com.dom.tsvpn"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
